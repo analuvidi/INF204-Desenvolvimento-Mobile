ff98c28c53ea409983ff0644a7c02213320765f2
Ana Luiza Vidigal <analvidigal@gmail.com>
2026-08-27 19:41:12 -0300

    feat: estruturar cartão de perfil
    Cria a estrutura inicial do cartão de perfil utilizando os componentes View, Text e Image, conforme o Passo 1 da prática.

--- projeto/App.js
+++ projeto/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  return(
+    <View style={StyleSheet.container}>
+      <View>
+        {/*Imagem sla */}
+        <Image
+          source={{ uri: "https://imgs.search.brave.com/86qNcvR2VYjPouUTE6vJHIh-DRqpXfqLuShAAFpfQBY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/aXMtdGhlcmUtYS1z/b3VyY2UtZm9yLXRo/ZXNlLTNkLWNnaS1l/bW9qaXMtdjAtb3M5/Nm96Y245a29mMS5w/bmc_d2lkdGg9NTEy/JmZvcm1hdD1wbmcm/YXV0bz13ZWJwJnM9/N2EyMjAzNzlkOWNk/YTM2YmUwYjE3ZjRl/MTc0N2JiOTlkNWU5/NjhhNw"}}
+          style={StyleSheet.avatar}
+        />
+        <Text style={StyleSheet.nomeUsuario}>Ana Luiza</Text>
+        <Text style={StyleSheet.profissao}>Estudante ADS</Text>
+      </View>
     </View>
-  );
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

e171176f538cadddc74966469447022673290bf0
Ana Luiza Vidigal <analvidigal@gmail.com>
2026-08-27 19:47:30 -0300

    fix: corrigir aplicação dos estilos do cartão
    Corrige as referências para styles, adiciona a estilização do cartão e ajusta a estrutura do JSX.

--- projeto/App.js
+++ projeto/App.js
@@ -3,15 +3,15 @@ import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return(
-    <View style={StyleSheet.container}>
-      <View>
-        {/*Imagem sla */}
+    <View style={styles.container}>
+      <View style={styles.cartao}>
+        {/* Imagem sla */}
         <Image
           source={{ uri: "https://imgs.search.brave.com/86qNcvR2VYjPouUTE6vJHIh-DRqpXfqLuShAAFpfQBY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/aXMtdGhlcmUtYS1z/b3VyY2UtZm9yLXRo/ZXNlLTNkLWNnaS1l/bW9qaXMtdjAtb3M5/Nm96Y245a29mMS5w/bmc_d2lkdGg9NTEy/JmZvcm1hdD1wbmcm/YXV0bz13ZWJwJnM9/N2EyMjAzNzlkOWNk/YTM2YmUwYjE3ZjRl/MTc0N2JiOTlkNWU5/NjhhNw"}}
-          style={StyleSheet.avatar}
+          style={styles.avatar}
         />
-        <Text style={StyleSheet.nomeUsuario}>Ana Luiza</Text>
-        <Text style={StyleSheet.profissao}>Estudante ADS</Text>
+        <Text style={styles.nomeUsuario}>Ana Luiza Vidigal</Text>
+        <Text style={styles.profissao}>Estudante ADS</Text>
       </View>
     </View>
 );

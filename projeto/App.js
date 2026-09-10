2e1aef3779840c87cb84d0e486cf0d97d03da2a6
Ana Luiza Vidigal <analvidigal@gmail.com>
2026-09-10 19:26:07 -0300

    feat: implementa mutação segura e interatividade
    Adiciona os botões de votação utilizando callback prev => prev + 1 no setState.

--- projeto/App.js
+++ projeto/App.js
@@ -8,7 +8,41 @@ export default function UrnaEletronica() {
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Painel de Botão</Text>
+
+      {/* Candidato A */}
+      <View style={styles.candidatoContainer}>
+        <Text style={styles.nomeCandidato}>Candidato A: {votosA} votos</Text>
+        <TouchableOpacity
+          style={styles.botaoVotar}
+          onPress={() => setVotosA(prev => prev + 1)}
+        >
+          <Text style ={styles.textoBotao}>Votar em A</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Candidato B */}
+      <View style={styles.candidatoContainer}>
+        <Text style={styles.nomeCandidato}>Candidato B: {votosB} votos</Text>
+        <TouchableOpacity
+          style={styles.botaoVotar}
+          onPress={() => setVotosB(prev => prev + 1)}
+        >
+          <Text style ={styles.textoBotao}>Votar em B</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Candidato C */}
+      <View style={styles.candidatoContainer}>
+        <Text style={styles.nomeCandidato}>Candidato C: {votosC} votos</Text>
+        <TouchableOpacity
+          style={styles.botaoVotar}
+          onPress={() => setVotosC(prev => prev + 1)}
+        >
+          <Text style ={styles.textoBotao}>Votar em C</Text>
+        </TouchableOpacity>
+      </View>
     </View>
+  
   );
 }
 
